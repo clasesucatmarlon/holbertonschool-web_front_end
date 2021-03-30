d0f8e272990d0b1852c6495b5d6ab88e9ff9edc7
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-03-30 10:08:50 -0500

    Count numbers primes into 2 and 100

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+    let primes = 0;
+    let isprime = true;
+    for (let counter = 2; counter <= 100; counter++) {
+        isprime = true;
+        let limit = Math.round(Math.sqrt(counter));
+        for (let mod = 2; mod <= limit; mod++) {
+            if (counter % mod == 0) {
+                isprime = false;
+                break;
+            }
+        }
+        if (isprime) {
+            primes = primes + 1;
+        }
+
+    }
+    console.log(primes);
+    return primes;
+}
+
+let startTime = window.performance.now();
+countPrimeNumbers();
+let endTime = window.performance.now();
+
+console.log("Execution time of printing countPrimeNumbers was " + (endTime - startTime) + " milliseconds.");
