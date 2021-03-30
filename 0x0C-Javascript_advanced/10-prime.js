1a63cd2f8375b375717acccf1fa57e9dea229a46
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-03-30 10:11:35 -0500

    Count numbers primes 100 times - Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+function countPrimeNumbers() {
+	let primes=0; 
+	let isprime = true;
+	for (let counter = 2; counter <= 100; counter++)
+	{
+		isprime = true;
+		let limit = Math.round(Math.sqrt(counter)); 
+		for (let mod = 2; mod <= limit; mod++) {
+			if (counter%mod == 0) {
+				isprime = false;
+				break; 
+			}
+		}
+		if (isprime) {
+			primes = primes + 1;
+		}
+
+	}
+	console.log(primes);
+	return primes;
+}
+
+let startTime = window.performance.now();
+
+// Count 100 times
+for (let i = 0; i < 100; i++) {
+	countPrimeNumbers();
+}
+let endTime = window.performance.now();
+
+console.log("Execution time of calculating prime numbers 100 times was " + (endTime - startTime));
